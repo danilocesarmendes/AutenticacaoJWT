89bc65fbfa53752c296ddb72fff983e699281c7f
Danilo Mendes <danilocesarmendes@gmail.com>
2019-07-27 10:39:15 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import authMiddleware from './app/middlewares/authMiddleware';
+
+import SessionsController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.get('/users', UserController.index);
+routes.delete('/users/:id', UserController.delete);
+
+export default routes;
